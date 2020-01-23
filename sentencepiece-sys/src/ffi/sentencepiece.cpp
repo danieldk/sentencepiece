99be6d534374c73990c5ce556a32ec469b9a48af
Daniël de Kok <me@danieldk.eu>
2020-01-23 17:14:56 +0100

    Add the sentencepiece-sys crate
    This crate provides small C wrappers around the sentencepiece C++
    library and uses bindgen to make the C functions callable from Rust.

---
+++ sentencepiece-sys/src/ffi/sentencepiece.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+
+#include <sentencepiece_processor.h>
+
+using sentencepiece::SentencePieceProcessor;
+using sentencepiece::SentencePieceText;
+
+extern "C" {
+
+SentencePieceProcessor *spp_new() {
+  return new SentencePieceProcessor();
+}
+
+unsigned char *spp_encode_as_serialized_proto(SentencePieceProcessor *spp, char const *sentence, size_t *len) {
+  auto serialized = spp->EncodeAsSerializedProto(sentence);
+
+  *len = serialized.size();
+  unsigned char *data = (unsigned char *) malloc(serialized.size());
+  memcpy(data, serialized.data(), serialized.size());
+
+  return data;
+}
+
+int spp_load(SentencePieceProcessor *spp, char const *filename) {
+  auto status = spp->Load(filename);
+  return status.code();
+}
+
+void spp_free(SentencePieceProcessor *spp) {
+  delete spp;
+}
+
+}
