b2d03c1a8824b21a7a9b7ab70b8c29263a2b4009
Systemcluster <me@systemcluster.me>
2023-07-15 05:15:59 +0200

    Add support for windows builds

--- sentencepiece-sys/build.rs
+++ sentencepiece-sys/build.rs
@@ -1,14 +1,32 @@
 use std::env;
 
 use cc::Build;
+use cmake::Config;
 
 macro_rules! feature(($name:expr) => (env::var(concat!("CARGO_FEATURE_", $name)).is_ok()));
 
 fn build_sentencepiece(builder: &mut Build) {
-    let dst = cmake::build("source");
+    let mut config = Config::new("source");
+    if builder.get_compiler().is_like_msvc() {
+        config.profile("Release");
+        if env::var("CARGO_CFG_TARGET_FEATURE")
+            .unwrap_or_default()
+            .contains("crt-static")
+        {
+            config.define("SPM_ENABLE_MSVC_MT_BUILD", "ON");
+            config.define("MSVC_RUNTIME_LIBRARY", "MultiThreaded");
+            config.define("CMAKE_MSVC_RUNTIME_LIBRARY", "MultiThreaded");
+            config.static_crt(true);
+        }
+    }
+    let dst = config.build();
     println!(
         "cargo:rustc-link-search=native={}",
         dst.join("build").join("src").display()
+    );
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
     );
     println!("cargo:rustc-link-lib=static=sentencepiece");
 
@@ -42,6 +60,12 @@ fn main() {
 
     if builder.get_compiler().is_like_msvc() {
         builder.flag("/std:c++17");
+        // same as https://github.com/google/sentencepiece/blob/master/CMakeLists.txt#L82C21-L82C28
+        builder.flag("/wd4267");
+        builder.flag("/wd4244");
+        builder.flag("/wd4305");
+        builder.flag("/Zc:strictStrings");
+        builder.flag("/utf-8");
     } else {
         builder.flag("-std=c++17");
     }
